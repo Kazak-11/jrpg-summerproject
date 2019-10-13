0e264cd8c19cbe3f4f6385fbfca2e4a86e8a85b5
KaiserProger <48857089+KaiserProger@users.noreply.github.com>
2019-10-13 21:31:11 +0700

    Update questModule.py

--- questModule.py
+++ questModule.py
@@ -1,2 +1,26 @@
 class Quest: # Класс квестов
-  pass
+  quest_name = ""
+  quest_desc = ""
+  quest_requirements = []
+  quest_reward = ""
+  def parse_arr(arr): # Парсер строки с триггерами выполнения
+    spreadsheet = ""
+    for i in arr:
+      i_s = i.split(" ")
+      o = {}
+      if "move" in i_s:
+        a = i_s.index("move")
+        if a % 2 == 0:
+          coords = list(int(i_s[a+1].strip().split(","))
+          x = coords[0]
+          y = coords[1]
+        else:
+          coords = list(int(i+s[a-1].strip().split(","))
+          x = coords[0]
+          y = coords[1]
+          o.update({"move", [x, y]})
+  def __init__(self, name, desc, requirements, reward):
+    self.name = name
+    self.desc = desc
+    self.requirements = requirements
+    self.reward = reward
